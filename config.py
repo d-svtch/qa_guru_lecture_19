a2be269898c47cfbb8d686181b8e41ae15e6692d
Dmitriy Vasilyev <61234128+d-svtch@users.noreply.github.com>
2025-05-06 12:17:04 +0300

    - minor editing

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
 context = os.getenv('context', 'bstack')
-bstack_userName = os.getenv('bstack_userName', 'iakivkramarenko_sKlOLN')
-bstack_accessKey = os.getenv('bstack_accessKey', 'FSHAmKdKHs3XsDkg35zT')
+bstack_userName = os.getenv('bstack_userName', 'bsuser_ePkJ0T')
+bstack_accessKey = os.getenv('bstack_accessKey', 'yVt6mmkgKhfs7o8PYzkx')
