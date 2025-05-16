f5be1418044ae3534df5bc1b4f6a186766318d55
d-svtch <s100tvdm@mgail.com>
2025-05-16 09:11:05 +0300

    -small preps

--- config.py
+++ config.py
@@ -1,47 +1,44 @@
 import os
-
-from selene_in_action import utils
-
-# context = os.getenv('context', 'bstack')
-# run_on_bstack = os.getenv('run_on_bstack', 'false').lower() == 'true'
-remote_url = os.getenv('remote_url', 'http://127.0.0.1:4723/')
-deviceName = os.getenv('deviceName')
-appWaitActivity = os.getenv('appWaitActivity', 'org.wikipedia.*')
-app = os.getenv('app', './app-alpha-universal-release.apk')
-runs_on_bstack = app.startswith('bs://')
-if runs_on_bstack:
-    remote_url = 'http://hub.browserstack.com/wd/hub'
-bstack_userName = os.getenv('bstack_userName', 'iakivkramarenko_qKHOLN')
-bstack_accessKey = os.getenv('bstack_accessKey', 'FSHAmndKHW3XsDkgm5zT')
+from dotenv import load_dotenv
+from appium.options.android import UiAutomator2Options
+import utils
 
 
-def to_driver_options():
-    from appium.options.android import UiAutomator2Options
+def to_driver_options(context):
     options = UiAutomator2Options()
 
-    if deviceName:
-        options.set_capability('deviceName', deviceName)
+    if context == 'local_emulator':
+        options.set_capability('remote_url', os.getenv('REMOTE_URL'))  # адрес удаленного сервера
+        options.set_capability('deviceName', os.getenv('DEVICE_NAME'))  # имя устройства
+        options.set_capability('appWaitActivity', os.getenv(
+            'APP_WAIT_ACTIVITY'))  # активити, которая будет открыта после запуска apk файла
+        # options.set_capability('udid', os.getenv('UDID')) # уникальный идентификатор устройства
+        options.set_capability('app', utils.file.abs_path_from_project(os.getenv('APP')))  # путь до apk файла
 
-    if appWaitActivity:
-        options.set_capability('appWaitActivity', appWaitActivity)
+    if context == 'local_real_device':
+        options.set_capability('remote_url', os.getenv('REMOTE_URL'))
+        options.set_capability('deviceName', os.getenv('DEVICE_NAME'))
+        options.set_capability('appWaitActivity', os.getenv('APP_WAIT_ACTIVITY'))
+        # options.set_capability('udid', os.getenv('UDID'))
+        options.set_capability('app', utils.file.path_from_project(os.getenv('APP')))
 
-    options.set_capability('app', (
-        app if (app.startswith('/') or runs_on_bstack)
-        else utils.file.abs_path_from_project(app)
-    ))
-
-    if runs_on_bstack:
-        options.set_capability('platformVersion', '9.0')
+    if context == 'bstack':
+        options.set_capability('remote_url', os.getenv('REMOTE_URL'))
+        options.set_capability('deviceName', os.getenv('DEVICE_NAME'))
+        options.set_capability('platformName', os.getenv('PLATFORM_NAME'))
+        options.set_capability('platformVersion', os.getenv('PLATFORM_VERSION'))
+        options.set_capability('appWaitActivity', os.getenv('APP_WAIT_ACTIVITY'))
+        options.set_capability('app', os.getenv('APP'))
+        load_dotenv(dotenv_path=utils.file.path_from_project(
+            '.env.credentials'))  # загрузка переменных окружения из файла .env.credentials
         options.set_capability(
             'bstack:options', {
-                'projectName': 'First Python project',
+                'projectName': 'Wikipedia project',
                 'buildName': 'browserstack-build-1',
-                'sessionName': 'BStack first_test',
-
-                'userName': bstack_userName,
-                'accessKey': bstack_accessKey,
+                'sessionName': 'BStack test',
+                'userName': os.getenv('USER_NAME'),
+                'accessKey': os.getenv('ACCESS_KEY'),
             },
         )
 
     return options
-
